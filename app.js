7faa565a67e7a4188c1242cf8a25e5e76797e697
koldovsky <koldovsky@koldovsky.com>
2022-09-27 20:05:59 +0300

    Pug code from course

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const admin = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const rootDir = require('./util/root-dir');
-
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(rootDir, 'public')));
-
-app.use('/admin', admin.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-
-app.use( (_, res) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-})
-
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
 const path = require('path');
+
 const express = require('express');
 
-const rootDir = require('../util/root-dir.js');
-const admin = require('./admin');
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log(admin.products);
-    res.sendFile( path.join(rootDir, 'views', 'shop.html') );
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
