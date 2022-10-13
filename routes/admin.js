be1547e224d165eb381f7ae28811ecce3669eece
koldovsky <koldovsky@koldovsky.com>
2022-10-13 20:03:40 +0300

    Refactoring

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 exports.routes = router;
