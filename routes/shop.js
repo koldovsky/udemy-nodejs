9e90fcbc0c4929fe8acf409c9071ba142fe1285c
koldovsky <koldovsky@koldovsky.com>
2022-10-20 19:15:09 +0300

    Starting code

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,19 @@
+const path = require('path');
+
 const express = require('express');
-const router = express.Router();
 
 const shopController = require('../controllers/shop');
 
-router.get('/', shopController.getIndex);
+const router = express.Router();
 
-router.get('/cart', shopController.getCart);
+router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/checkout', shopController.getCheckout);
+router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
